80998430ad3e29d32d880b286d5a2aa8c210b703
Dalmontron05 <vexr2dts85338@gmail.com>
2024-09-12 18:23:05 -0700

    hello world

---
+++ SpaceX-IO/SpaceX.java
@@ -0,0 +1,13 @@
+// Import scanner class to read input from our user
+import java.util.Scanner;
+
+// Make sure class name is the same as filename
+public class SpaceX
+{
+    // Main method to run the program
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
+
