bd4e115f09e18b00448e3ce0a1713a47f273d3b4
Dalmontron05 <vexr2dts85338@gmail.com>
2024-09-16 00:24:50 -0700

    finished introduction

--- SpaceX-IO/Main.java
+++ SpaceX-IO/Main.java
@@ -8,13 +8,19 @@ public class Main
     public static void main(String[] args)
     {
         try (Scanner in = new Scanner(System.in)) {
+            // introduction
+            System.out.println("Hey man, welcome to the space station! Glad to see you got here safely. I know you're probably excited about being in zero gravity and all, but once you get settled in there's a safety briefing that's been prepared for you just over there. Let us know when you're ready for that.\n");
+
             // asks for user input
-            System.out.println("What's your name?");
+            System.out.println("Oh, sorry I got a little ahead of myself. I'm Luke. You were...?");
+
             // reads the user input
             String name = in.nextLine();
+
             // prints user input
-            System.out.println("We're happy to have you on board, " + name + "!");
-            in.close();
+            System.out.println("\nWe're happy to have you on board, " + name + "!");
+
+            in.close(); //? why does this not work in a finally block with the try method
         }
     }
 }
