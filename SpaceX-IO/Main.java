3cba4acc468fd8b71570b43b4ed5961766a853e5
Dalmontron05 <vexr2dts85338@gmail.com>
2024-09-17 18:13:22 -0700

    figured out scanner close method

--- SpaceX-IO/Main.java
+++ SpaceX-IO/Main.java
@@ -1,3 +1,5 @@
+//* Main Java File
+
 // Import scanner class to read input from our user
 import java.util.Scanner;
 
@@ -7,21 +9,20 @@ public class Main
     // Main method to run the program
     public static void main(String[] args)
     {
-        try (Scanner in = new Scanner(System.in)) {
-            // introduction
-            System.out.println("Hey man, welcome to the space station! Glad to see you got here safely. I know you're probably excited about being in zero gravity and all, but once you get settled in there's a safety briefing that's been prepared for you just over there. Let us know when you're ready for that.\n");
+        Scanner in = new Scanner(System.in);
+        // introduction
+        System.out.println("Hey man, welcome to the space station! Glad to see you got here safely. I know you're probably excited about being in zero gravity and all, but once you get settled in there's a safety briefing that's been prepared for you just over there. Let us know when you're ready for that.\n");
 
-            // asks for user input
-            System.out.println("Oh, sorry I got a little ahead of myself. I'm Luke. You were...?");
+        // asks for user input
+        System.out.println("Oh, sorry I got a little ahead of myself. I'm Luke. You were...?");
 
-            // reads the user input
-            String name = in.nextLine();
+        // reads the user input
+        String name = in.nextLine();
 
-            // prints user input
-            System.out.println("\nWe're happy to have you on board, " + name + "!");
+        // prints user input
+        System.out.println("\nWe're happy to have you on board, " + name + "!");
 
-            in.close(); //? why does this not work in a finally block with the try method
-        }
+        in.close();
     }
 }
 
