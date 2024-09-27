1b62808747c9d9c1529aaf29bea42dc15f9bba6b
Dalmontron05 <vexr2dts85338@gmail.com>
2024-09-27 10:38:15 -0700

    removed unecessary scanner class for now

--- Mars-Energy-Grid-Simulator/MarsBaseEnergyGrid.java
+++ Mars-Energy-Grid-Simulator/MarsBaseEnergyGrid.java
@@ -1,31 +1,34 @@
 //* Main Java File
 
 // Imports
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class MarsBaseEnergyGrid
 {
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
+        // Scanner in = new Scanner(System.in);
 
         // Introduction
         System.out.println("Welcome to the Mars Base Energy Grid Manage!");
         System.out.println("This Program will help you manage the energy grid for the Mars base.");
         System.out.println("I see you are the new commander, let's get started!");
 
+        // Variables
         int[] solarPanels = {30,45,25,35,40};
         int [] windTurbines = {20,30,25,40,35};
 
+        // Adds all values in the two arrays to two variables. then adds the total of both arrays to one variable.
         int totalSolarEnergy = calculateTotalEnergy(solarPanels);
         int totalWindEnergy = calculateTotalEnergy(windTurbines);
         int combinedTotalEnergy = totalSolarEnergy + totalWindEnergy;
 
-        System.out.println("total solar energy:" + totalSolarEnergy);
-        System.out.println("total wind energy:" + totalWindEnergy);
-        System.out.println("total energy:" + combinedTotalEnergy);
+        // prints totaled variables
+        System.out.println("Total Solar Energy:" + totalSolarEnergy);
+        System.out.println("Total Wind Energy:" + totalWindEnergy);
+        System.out.println("Total Energy:" + combinedTotalEnergy);
         
-        in.close();
+        // in.close();
     }
 
     public static int calculateTotalEnergy(int[] energySources)
@@ -33,6 +36,7 @@ public class MarsBaseEnergyGrid
         int totalEnergy = 0;
 
         // loops through the energy sources and add the energy output to the total energy
+        // for-each loop: "eachEnergySource" acts as the value for the index, not the index itself.
         for(int eachEnergySource : energySources)
         {
             // add the energy output of the current energy source to the total energy
