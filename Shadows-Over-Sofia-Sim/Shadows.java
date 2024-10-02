82c8e2f16d441a147a89322feead0d96a82ab05b
Dalmontron05 <vexr2dts85338@gmail.com>
2024-10-01 18:22:21 -0700

    did shadows class demo; started horror story

---
+++ Shadows-Over-Sofia-Sim/Shadows.java
@@ -0,0 +1,25 @@
+//* Main Java File
+
+// Imports
+// import java.util.Scanner;
+
+
+public class Shadows
+{
+    public static void main(String[] args)
+    {
+        displayTitle();
+        displayDescription();
+    }
+
+    public static void displayTitle()
+    {
+        System.out.println("\n\n\t *** Shadows Over Sofia Crime Sim *** \n\n");
+    }
+
+    public static void displayDescription()
+    {
+        System.out.println("\na cool description");
+    }
+}
+
---
+++ horror-walking-simulator/HorrorWalkingSimulator.java
@@ -0,0 +1,23 @@
+//* Main Java File
+
+// Imports
+// import java.util.Scanner;
+
+
+public class HorrorWalkingSimulator
+{
+    public static void main(String[] args)
+    {
+        // Scanner in = new Scanner(System.in);
+
+        // Print colored text to the console
+        // System.out.println(GREEN + "This text is green." + RESET);
+        // System.out.println(YELLOW + "This text is yellow." + RESET);
+        // System.out.println(RED + "This text is red." + RESET);
+        
+        System.out.println("Hello, this is an interactive choose your own adventure story inspired by various horror cryptids.");
+
+        // in.close();
+    }
+}
+
