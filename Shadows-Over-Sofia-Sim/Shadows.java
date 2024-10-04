b3d3d80626f39acbfe6fb8a4e0c01e717475051b
Dalmontron05 <vexr2dts85338@gmail.com>
2024-10-03 18:21:22 -0700

    kinda did my own thing for shadows demo pt 2

--- Shadows-Over-Sofia-Sim/Shadows.java
+++ Shadows-Over-Sofia-Sim/Shadows.java
@@ -1,7 +1,7 @@
 //* Main Java File
 
 // Imports
-// import java.util.Scanner;
+import java.util.Scanner;
 
 
 public class Shadows
@@ -20,6 +20,42 @@ public class Shadows
     public static void displayDescription()
     {
         System.out.println("\na cool description");
+    }
+
+    // Senario 1: The Unsettling Telegram
+    public static int Senario1()
+    {
+        // Introduction
+        System.out.println("\n***The Unsettling Telegram***\n");
+        
+        // Choices
+        System.out.println("1. Choice 1");
+        
+        System.out.println("2. Choice 2");
+        
+        System.out.println("3. Choice 3");
+        
+        Scanner in = new Scanner(System.in);
+        int answer;
+        answer = in.nextInt();
+        in.close();
+
+        switch (answer)
+        {
+            case 1:
+                System.out.println("Stuff after you made choice 1...");
+                break;
+            case 2:
+                System.out.println("Stuff after you made choice 2...");
+                break;
+            case 3:
+                System.out.println("Stuff after you made choice 3...");
+                break;
+            default:
+                System.err.println("Invalid choice");
+        }
+
+        return answer;
     }
 }
 
